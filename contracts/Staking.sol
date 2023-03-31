a73cb7e9e783aee4f2595ffe123dc6cf875c005e
fiaew <arjanvanhoogdalem@gmail.com>
2023-03-31 18:09:11 +0200

    first modifications, not tested

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,18 +3,13 @@ pragma solidity ^0.8;
 
 contract Staking {
     IERC20 public immutable stakingToken;
-    IERC20 public immutable rewardsToken;
 
+    /* ========== STATE VARIABLES ========== */
     address public owner;
 
-    // Duration of rewards to be paid out (in seconds)
-    uint public duration;
-    // Timestamp of when the rewards finish
-    uint public finishAt;
-    // Minimum of last updated time and reward finish time
-    uint public updatedAt;
-    // Reward to be paid out per second
-    uint public rewardRate;
+    // Casino profit for staking period
+    uint256 public casinoProfit;
+
     // Sum of (reward rate * dt * 1e18 / total supply)
     uint public rewardPerTokenStored;
     // User address => rewardPerTokenStored
@@ -27,11 +22,14 @@ contract Staking {
     // User address => staked amount
     mapping(address => uint) public balanceOf;
 
-    constructor(address _stakingToken, address _rewardToken) {
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(address _stakingToken) {
         owner = msg.sender;
         stakingToken = IERC20(_stakingToken);
-        rewardsToken = IERC20(_rewardToken);
     }
+
+    /* ========== MODIFIERS ========== */
 
     modifier onlyOwner() {
         require(msg.sender == owner, "not authorized");
@@ -40,30 +38,30 @@ contract Staking {
 
     modifier updateReward(address _account) {
         rewardPerTokenStored = rewardPerToken();
-        updatedAt = lastTimeRewardApplicable();
-
         if (_account != address(0)) {
             rewards[_account] = earned(_account);
             userRewardPerTokenPaid[_account] = rewardPerTokenStored;
         }
-
         _;
     }
+    
+    /* ========== VIEWS ========== */
 
-    function lastTimeRewardApplicable() public view returns (uint) {
-        return _min(finishAt, block.timestamp);
-    }
-
-    function rewardPerToken() public view returns (uint) {
+    function rewardPerToken() public view returns (uint256) {
         if (totalSupply == 0) {
-            return rewardPerTokenStored;
+            return 0;
         }
-
-        return
-            rewardPerTokenStored +
-            (rewardRate * (lastTimeRewardApplicable() - updatedAt) * 1e18) /
-            totalSupply;
+        return casinoProfit / totalSupply;
     }
+
+    function earned(address _account) public view returns (uint) {
+        return
+            ((balanceOf[_account] *
+                (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
+            rewards[_account];
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
 
     function stake(uint _amount) external updateReward(msg.sender) {
         require(_amount > 0, "amount = 0");
@@ -79,49 +77,35 @@ contract Staking {
         stakingToken.transfer(msg.sender, _amount);
     }
 
-    function earned(address _account) public view returns (uint) {
-        return
-            ((balanceOf[_account] *
-                (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
-            rewards[_account];
-    }
 
     function getReward() external updateReward(msg.sender) {
         uint reward = rewards[msg.sender];
         if (reward > 0) {
             rewards[msg.sender] = 0;
-            rewardsToken.transfer(msg.sender, reward);
+            stakingToken.transfer(msg.sender, reward);
         }
     }
 
-    function setRewardsDuration(uint _duration) external onlyOwner {
-        require(finishAt < block.timestamp, "reward duration not finished");
-        duration = _duration;
-    }
+    /* ========== RESTRICTED FUNCTIONS ========== */
 
-    function notifyRewardAmount(
-        uint _amount
-    ) external onlyOwner updateReward(address(0)) {
-        if (block.timestamp >= finishAt) {
-            rewardRate = _amount / duration;
-        } else {
-            uint remainingRewards = (finishAt - block.timestamp) * rewardRate;
-            rewardRate = (_amount + remainingRewards) / duration;
-        }
+    // function notifyRewardAmount(uint _amount) external onlyOwner updateReward(address(0)) {
+    //     if (block.timestamp >= finishAt) {
+    //         rewardRate = _amount / duration;
+    //     } else {
+    //         uint remainingRewards = (finishAt - block.timestamp) * rewardRate;
+    //         rewardRate = (_amount + remainingRewards) / duration;
+    //     }
 
-        require(rewardRate > 0, "reward rate = 0");
-        require(
-            rewardRate * duration <= rewardsToken.balanceOf(address(this)),
-            "reward amount > balance"
-        );
+    //     require(rewardRate > 0, "reward rate = 0");
+    //     require(
+    //         rewardRate * duration <= rewardsToken.balanceOf(address(this)),
+    //         "reward amount > balance"
+    //     );
 
-        finishAt = block.timestamp + duration;
-        updatedAt = block.timestamp;
-    }
+    //     finishAt = block.timestamp + duration;
+    //     updatedAt = block.timestamp;
+    // }
 
-    function _min(uint x, uint y) private pure returns (uint) {
-        return x <= y ? x : y;
-    }
 }
 
 interface IERC20 {
