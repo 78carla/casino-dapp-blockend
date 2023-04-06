ab0b197ad3397892ca78f171732df2f9041044d5
fiaew <arjanvanhoogdalem@gmail.com>
2023-04-06 09:57:10 +0200

    correct setup for mumbai

--- scripts/Casino.ts
+++ scripts/Casino.ts
@@ -32,7 +32,7 @@ const STAKE_AMOUNT = 2;
 async function main() {
   // Connect to the network
   const provider = new ethers.providers.InfuraProvider(
-    "sepolia",
+    "maticmum",
     process.env.INFURA_API_KEY_SEPOLIA
   );
 
@@ -54,7 +54,7 @@ async function main() {
   //const contractFactory = new Lottery__factory(accounts[0]);
   const contractFactory = new Casino__factory(signer);
   const contract = contractFactory.attach(
-    "0x6B7Df6c8B03F82aa7bdeCfb34E7008147E18d095"
+    "0x16e67e83163d64d4d030a4c9d0a25135678c042a"
   );
 
   const tokenAddress = await contract.token();
@@ -91,152 +91,109 @@ async function main() {
   // );
 
 
-  //Check the T7E balance of the signer after the purchase
-  const tokenBalanceBN = await token.balanceOf(signer.address);
-  const tokenBalance = ethers.utils.formatEther(tokenBalanceBN);
-  console.log(`The ${signer.address} account has now ${tokenBalance} T7E\n`);
+  // //Check the T7E balance of the signer after the purchase
+  // const tokenBalanceBN = await token.balanceOf(signer.address);
+  // const tokenBalance = ethers.utils.formatEther(tokenBalanceBN);
+  // console.log(`The ${signer.address} account has now ${tokenBalance} T7E\n`);
 
-  //The signer allows the Casino contract to spend the T7E tokens
-  const allowTx = await token
-    .connect(signer)
-    .approve(contract.address, ethers.constants.MaxUint256);
-  const allowTxReceipt = await allowTx.wait();
-  console.log("Allowance confirmed at block", allowTxReceipt.blockNumber, "\n");
-
-
-    //Fund contract with T7E via staking
-  //stake some tokens
-  console.log("staking tokens");
-  const stakingTx = await contract.stake(ethers.utils.parseEther(STAKE_AMOUNT.toString()));
-  await stakingTx.wait();
-
-  // const buyNFTTx = await contract.connect(signer).purchaseNft();
-  // const buyNFTReceipt = await buyNFTTx.wait();
-  // console.log("BOUGHT NFT");
-
-  //Play the game and flip the coin
-  const flipTx = await contract.connect(signer).flipCoinRigged();
-  console.log("FLIP WAIT ");
-  const flipTxReceipt = await flipTx.wait();
-  console.log(
-    "The Flip was confermed at block number",
-    flipTxReceipt.blockNumber,
-    "\n"
-  );
+  // //The signer allows the Casino contract to spend the T7E tokens
+  // const allowTx = await token
+  //   .connect(signer)
+  //   .approve(contract.address, ethers.constants.MaxUint256);
+  // const allowTxReceipt = await allowTx.wait();
+  // console.log("Allowance confirmed at block", allowTxReceipt.blockNumber, "\n");
 
 
-  //Return the pool size after the play
-  const prizePoolAfter = await contract.totalSupply();
-  console.log(
-    "After the Flip the Prize pool contains: ",
-    ethers.utils.formatEther(prizePoolAfter),
-    "T7E"
-  );
+  //   //Fund contract with T7E via staking
+  // //stake some tokens
+  // console.log("staking tokens");
+  // const stakingTx = await contract.stake(ethers.utils.parseEther(STAKE_AMOUNT.toString()));
+  // await stakingTx.wait();
 
-  //Play the game and flip the coin
-  const flipTx2 = await contract.connect(signer).flipCoinRigged();
-  console.log("FLIP WAIT ");
-  const flipTxReceipt2 = await flipTx.wait();
-  console.log(
-    "The Flip was confermed at block number",
-    flipTxReceipt.blockNumber,
-    "\n"
-  );
+  // // const buyNFTTx = await contract.connect(signer).purchaseNft();
+  // // const buyNFTReceipt = await buyNFTTx.wait();
+  // // console.log("BOUGHT NFT");
 
-  //Return the pool size after the play
-  const prizePoolAfter2 = await contract.totalSupply();
-  console.log(
-    "After the Flip the Prize pool contains: ",
-    ethers.utils.formatEther(prizePoolAfter),
-    "T7E"
-  );
-  //Play the game and flip the coin
-  const flipTx3 = await contract.connect(signer).flipCoinRigged();
-  console.log("FLIP WAIT ");
-  const flipTxReceipt3 = await flipTx.wait();
-  console.log(
-    "The Flip was confermed at block number",
-    flipTxReceipt.blockNumber,
-    "\n"
-  );
+  // //Play the game and flip the coin
+  // const flipTx = await contract.connect(signer).flipCoinRigged();
+  // console.log("FLIP WAIT ");
+  // const flipTxReceipt = await flipTx.wait();
+  // console.log(
+  //   "The Flip was confermed at block number",
+  //   flipTxReceipt.blockNumber,
+  //   "\n"
+  // );
 
-  //unstake some tokens
-  console.log("staking tokens");
-  const unstakingTx = await contract.unstake(ethers.utils.parseEther(STAKE_AMOUNT.toString()));
-  await unstakingTx.wait();
 
-  //Return the pool size after the play
-  const prizePoolAfter3 = await contract.totalSupply();
-  console.log(
-    "After the Flip the Prize pool contains: ",
-    ethers.utils.formatEther(prizePoolAfter),
-    "T7E"
-  );
-
-  //Return the T7E balance of the signer after the play
-  const tokenBalanceBNAfter = await token.balanceOf(signer.address);
-  const tokenBalanceAfter = ethers.utils.formatEther(tokenBalanceBNAfter);
-  console.log(
-    `After the Flip the ${signer.address} account now has ${tokenBalanceAfter} T7E\n`
-  );
-
-  //Return the ETH balance of the signer after the play
-  const balanceBNAfter = await ethers.provider.getBalance(signer.address);
-  const balanceAfter = ethers.utils.formatEther(balanceBNAfter);
-  console.log(
-    `After the Flip the ${signer.address} account has ${balanceAfter} ETH\n`
-  );
-
-  //The owner can withdraw the prize pool
-  // const poolTx = await contract.ownerWithdraw(ethers.utils.parseEther("10"));
-  // const poolReceipt = await poolTx.wait();
-  // console.log(`Withdraw confirmed (${poolReceipt.transactionHash})\n`);
-
-  // //Return the pool size after the withdraw
-  // const prizePoolAfterWithdraw = await contract.prizePool();
+  // //Return the pool size after the play
+  // const prizePoolAfter = await contract.totalSupply();
   // console.log(
   //   "After the Flip the Prize pool contains: ",
-  //   ethers.utils.formatEther(prizePoolAfterWithdraw),
+  //   ethers.utils.formatEther(prizePoolAfter),
   //   "T7E"
   // );
 
-  // async function displayPrize(index: string): Promise<string> {
-  //   const prizeBN = await contract.prize(accounts[Number(index)].address);
-  //   const prize = ethers.utils.formatEther(prizeBN);
-  //   console.log(
-  //     `The account of address ${
-  //       accounts[Number(index)].address
-  //     } has earned a prize of ${prize} Tokens\n`
-  //   );
-  //   return prize;
-  // }
+  // //Play the game and flip the coin
+  // const flipTx2 = await contract.connect(signer).flipCoinRigged();
+  // console.log("FLIP WAIT ");
+  // const flipTxReceipt2 = await flipTx.wait();
+  // console.log(
+  //   "The Flip was confermed at block number",
+  //   flipTxReceipt.blockNumber,
+  //   "\n"
+  // );
 
-  // async function claimPrize(index: string, amount: string) {
-  //   const tx = await contract
-  //     .connect(accounts[Number(index)])
-  //     .prizeWithdraw(ethers.utils.parseEther(amount));
-  //   const receipt = await tx.wait();
-  //   console.log(`Prize claimed (${receipt.transactionHash})\n`);
-  // }
+  // //Return the pool size after the play
+  // const prizePoolAfter2 = await contract.totalSupply();
+  // console.log(
+  //   "After the Flip the Prize pool contains: ",
+  //   ethers.utils.formatEther(prizePoolAfter),
+  //   "T7E"
+  // );
+  // //Play the game and flip the coin
+  // const flipTx3 = await contract.connect(signer).flipCoinRigged();
+  // console.log("FLIP WAIT ");
+  // const flipTxReceipt3 = await flipTx.wait();
+  // console.log(
+  //   "The Flip was confermed at block number",
+  //   flipTxReceipt.blockNumber,
+  //   "\n"
+  // );
 
-  // async function displayOwnerPool() {
-  //   const balanceBN = await contract.ownerPool();
-  //   const balance = ethers.utils.formatEther(balanceBN);
-  //   console.log(`The owner pool has (${balance}) Tokens \n`);
-  // }
 
-  // async function burnTokens(index: string, amount: string) {
-  //   const allowTx = await token
-  //     .connect(accounts[Number(index)])
-  //     .approve(contract.address, ethers.constants.MaxUint256);
-  //   const receiptAllow = await allowTx.wait();
-  //   console.log(`Allowance confirmed (${receiptAllow.transactionHash})\n`);
-  //   const tx = await contract
-  //     .connect(accounts[Number(index)])
-  //     .returnTokens(ethers.utils.parseEther(amount));
-  //   const receipt = await tx.wait();
-  //   console.log(`Burn confirmed (${receipt.transactionHash})\n`);
-  // }
+  // //unstake some tokens
+  // console.log("unstaking tokens");
+  // const unstakingTx = await contract.unstakeAll();
+  // await unstakingTx.wait();
+
+
+  //unstake some tokens
+  console.log("unstaking tokens");
+  const unstakingTx = await contract.unstake(ethers.utils.parseEther("3"));
+  await unstakingTx.wait();
+
+  // //Return the pool size after the play
+  // const prizePoolAfter3 = await contract.totalSupply();
+  // console.log(
+  //   "After the Flip the Prize pool contains: ",
+  //   ethers.utils.formatEther(prizePoolAfter),
+  //   "T7E"
+  // );
+
+  // //Return the T7E balance of the signer after the play
+  // const tokenBalanceBNAfter = await token.balanceOf(signer.address);
+  // const tokenBalanceAfter = ethers.utils.formatEther(tokenBalanceBNAfter);
+  // console.log(
+  //   `After the Flip the ${signer.address} account now has ${tokenBalanceAfter} T7E\n`
+  // );
+
+  // //Return the ETH balance of the signer after the play
+  // const balanceBNAfter = await ethers.provider.getBalance(signer.address);
+  // const balanceAfter = ethers.utils.formatEther(balanceBNAfter);
+  // console.log(
+  //   `After the Flip the ${signer.address} account has ${balanceAfter} ETH\n`
+  // );
+
 }
 
 main().catch((error) => {
---
+++ scripts/Deploy.ts
@@ -0,0 +1,172 @@
+import { ethers } from "hardhat";
+import {
+  Casino,
+  CasinoToken,
+  CasinoPassport,
+  Casino__factory,
+  CasinoToken__factory,
+  CasinoPassport__factory
+} from "../typechain-types";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+let contract: Casino;
+let token: CasinoToken;
+let nft: CasinoPassport;
+//let accounts: SignerWithAddress[];
+
+let signer;
+
+const VERIFY_TEXT = "Verify the Casino contract with this command: \n npx hardhat verify --network "
+const NETWORK_NAME = "maticmum"
+
+const BUY_AMOUNT = 100;
+const STAKE_AMOUNT = 4;
+
+const PLAY_PRICE = 1;
+const TOKEN_RATIO = 10000;
+const PAYOUT_RATIO = 95;
+
+async function main() {
+  // Connect to the network
+  const provider = new ethers.providers.InfuraProvider(
+    NETWORK_NAME,
+    process.env.INFURA_API_KEY_SEPOLIA
+  );
+
+  // Get the private key from the environment variable
+  const privateKey = process.env.PRIVATE_KEY;
+  if (!privateKey || privateKey.length <= 0) {
+    throw new Error("Private key missing");
+  }
+
+  //Connect to the wallet
+  const wallet = new ethers.Wallet(privateKey);
+  console.log("Connected to the wallet address", wallet.address);
+  signer = wallet.connect(provider);
+
+  //signer = (await ethers.getSigners())[0];
+  console.log("The signer is", signer.address);
+
+  console.log("Deploying Casino contract:");
+  //const contractFactory = new Lottery__factory(accounts[0]);
+  const contractFactory = new Casino__factory(signer);
+  console.log("Deploying contract ...");
+
+  contract = await contractFactory.deploy(
+    TOKEN_RATIO,
+    ethers.utils.parseEther(PLAY_PRICE.toFixed(18)),
+    PAYOUT_RATIO
+  );
+
+  const tokenAddress = await contract.token();
+  const tokenFactory = new CasinoToken__factory(signer);
+  token = tokenFactory.attach(tokenAddress);
+
+  const nftAddress = await contract.nft();
+  const nftFactory = new CasinoPassport__factory(signer);
+  nft = nftFactory.attach(nftAddress);
+
+
+  const deployTransactionReceipt = await contract.deployTransaction.wait();
+  console.log(
+    "The Casino contract address is:",
+    contract.address,
+    "and was deployed by",
+    signer.address,
+    "at block number",
+    deployTransactionReceipt.blockNumber,
+    "\n"
+  );
+  console.log(
+    "The Token contract address is:",
+    token.address,
+    "and was deployed by",
+    signer.address,
+    "\n"
+  );
+  console.log(
+    "The NFT contract address is:",
+    nft.address,
+    "and was deployed by",
+    signer.address,
+    "\n"
+  );
+
+    //Buy some T7E tokens
+    // async function buyTokens(index: string, amount: string) {
+      const txBuy = await contract.connect(signer).purchaseTokens({
+        value: ethers.utils.parseEther(BUY_AMOUNT.toString()).div(TOKEN_RATIO),
+      });
+      const receiptBuy = await txBuy.wait();
+      console.log(
+        `Tokens bought at ${receiptBuy.transactionHash} transaction hash\n`
+      );
+
+  //The signer allows the Casino contract to spend the T7E tokens
+  const allowTx = await token
+    .connect(signer)
+    .approve(contract.address, ethers.constants.MaxUint256);
+  const allowTxReceipt = await allowTx.wait();
+  console.log("Allowance confirmed at block", allowTxReceipt.blockNumber, "\n");
+
+
+    //stake some tokens
+    console.log("staking tokens");
+    const stakingTx = await contract.stake(ethers.utils.parseEther(STAKE_AMOUNT.toString()));
+    await stakingTx.wait();
+    const tokenBalanceBN = await contract.totalSupply();
+    const tokenBalance = ethers.utils.formatEther(tokenBalanceBN);
+
+    console.log(`The casino contract with address ${contract.address} has ${tokenBalance} T7E\n`);
+
+  console.log(
+    `${VERIFY_TEXT} ${NETWORK_NAME} ${token.address} `
+  );
+
+  console.log(
+    `Verify the NFT contract with this command: \n
+    npx hardhat verify --network maticmum ${nft.address} 
+    `
+  );
+  console.log(
+    `Verify the Casino contract with this command: \n
+    npx hardhat verify --network maticmum ${
+      contract.address
+    } "${TOKEN_RATIO}" "${ethers.utils.parseEther(
+      PLAY_PRICE.toFixed(18)
+    )}" "${PAYOUT_RATIO}"
+    `
+  );
+
+
+
+  //Play the game and flip the coin
+  const flipTx = await contract.connect(signer).flipCoinRigged();
+  console.log("FLIP WAIT ");
+  const flipTxReceipt = await flipTx.wait();
+  console.log(
+    "The Flip was confermed at block number",
+    flipTxReceipt.blockNumber,
+    "\n"
+  );
+
+
+  
+  // //unstake some tokens
+  // console.log("unstaking tokens");
+  // const unstakingTx = await contract.unstake(ethers.utils.parseEther("1"));
+  // await unstakingTx.wait();
+
+  //unstake some tokens
+  console.log("unstaking tokens");
+  const unstakingTx = await contract.unstakeAll();
+  await unstakingTx.wait();
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
